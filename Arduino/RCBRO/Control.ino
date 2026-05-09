529da69dc323ca94bcfa31de6cb16b9a61e8768f
cc_mk <mk@circleconsult.dk>
2026-05-09 12:06:35 +0200

    Optimize controller

--- Arduino/RCBRO/Control.ino
+++ Arduino/RCBRO/Control.ino
@@ -12,6 +12,8 @@
  *
  */
 
+#include <util/atomic.h>
+
 /**
  * @brief     Stability control function. This is a PD controller that calculates the control output based on the current angle and the target angle.
  * @param     DT          Time step in seconds.
@@ -23,20 +25,19 @@
  */
 float stabilityPDControl(float DT, float input, float setPoint, float Kp, float Kd)
 {
+    if (DT <= 0.0f)
+        return 0.0f;
+
     float error = setPoint - input;
 
-    // Kd is implemented in two parts:
-    //   Sensor part: -(input - input(t-1))
-    //   Setpoint part: setPoint - setPoint(t-1)  (clamped to avoid aggressive jumps)
     float Kd_setPoint = constrain((setPoint - setPointOld), -8.0f, 8.0f);
-
-    // Kd factored out (saves one float multiply).
-    // 1/DT used instead of division (float divide is ~4x slower than multiply on AVR).
     float inv_DT = 1.0f / DT;
+
     float output = Kp * error + Kd * (Kd_setPoint - (input - PID_errorOld)) * inv_DT;
 
-    PID_errorOld = input;   // D-term uses measurement only, not error
+    PID_errorOld = input;
     setPointOld = setPoint;
+
     return output;
 }
 
@@ -53,14 +54,19 @@ float stabilityPDControl(float DT, float input, float setPoint, float Kp, float 
  */
 float speedPIControl(float DT, int16_t input, int16_t setPoint, float Kp, float Ki, bool integrate = true)
 {
+    if (DT <= 0.0f)
+        return 0.0f;
+
     int16_t error = setPoint - input;
+
     if (integrate)
     {
-        PID_errorSum += constrain(error, -ITERM_MAX_ERROR, ITERM_MAX_ERROR);
+        float limitedError = constrain((float)error, -ITERM_MAX_ERROR, ITERM_MAX_ERROR);
+        PID_errorSum += limitedError * DT;
         PID_errorSum = constrain(PID_errorSum, -ITERM_MAX, ITERM_MAX);
     }
 
-    return Kp * error + Ki * PID_errorSum * DT;
+    return Kp * error + Ki * PID_errorSum;
 }
 
 /**
@@ -75,7 +81,10 @@ float speedPIControl(float DT, int16_t input, int16_t setPoint, float Kp, float 
 float positionPDControl(long actualPos, long setPointPos, float Kpp, float Kdp, int16_t speedM)
 {
     float P = constrain(Kpp * (float)(setPointPos - actualPos), -115.0f, 115.0f);
-    return P + Kdp * (float)speedM;
+
+    // Damping should usually oppose movement.
+    // If your robot reacts backwards, change '-' back to '+'.
+    return P - Kdp * (float)speedM;
 }
 
 /**
@@ -89,7 +98,13 @@ ISR(TIMER1_COMPA_vect)
         return;
 
     SET(STEP_M1_PORT, STEP_M1_PIN);
-    asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
+
+    // Minimum STEP high pulse width for stepper driver
+    asm volatile(
+        "nop\nnop\nnop\nnop\n"
+        "nop\nnop\nnop\nnop\n"
+    );
+
     CLR(STEP_M1_PORT, STEP_M1_PIN);
 
     if (dir_M1 > 0)
@@ -109,7 +124,13 @@ ISR(TIMER3_COMPA_vect)
         return;
 
     SET(STEP_M2_PORT, STEP_M2_PIN);
-    asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
+
+    // Minimum STEP high pulse width for stepper driver
+    asm volatile(
+        "nop\nnop\nnop\nnop\n"
+        "nop\nnop\nnop\nnop\n"
+    );
+
     CLR(STEP_M2_PORT, STEP_M2_PIN);
 
     if (dir_M2 > 0)
@@ -128,10 +149,11 @@ void setMotorSpeed(uint8_t motorNum, int16_t tspeed)
 {
     int32_t speed;
     int32_t timer_period;
+
     int16_t         &speed_M = (motorNum == 1) ? speed_M1 : speed_M2;
     volatile int8_t &dir_M   = (motorNum == 1) ? dir_M1   : dir_M2;
 
-    // WE LIMIT MAX ACCELERATION of the motors
+    // Limit motor acceleration
     if ((speed_M - tspeed) > MAX_ACCEL)
         speed_M -= MAX_ACCEL;
     else if ((speed_M - tspeed) < -MAX_ACCEL)
@@ -139,10 +161,10 @@ void setMotorSpeed(uint8_t motorNum, int16_t tspeed)
     else
         speed_M = tspeed;
 
-#if MICROSTEPPING==16
-    speed = speed_M * 50;
+#if MICROSTEPPING == 16
+    speed = (int32_t)speed_M * 50L;
 #else
-    speed = speed_M * 25;
+    speed = (int32_t)speed_M * 25L;
 #endif
 
     if (speed == 0)
@@ -152,30 +174,61 @@ void setMotorSpeed(uint8_t motorNum, int16_t tspeed)
     }
     else if (speed > 0)
     {
-        timer_period = 2000000 / speed;
-        dir_M = 1;
-        if (motorNum == 1) SET(DIR_M1_PORT, DIR_M1_PIN);
-        else               CLR(DIR_M2_PORT, DIR_M2_PIN); // Motor2 forward = CLR
+        timer_period = 2000000L / speed;
+
+        if (timer_period > 65535L)
+        {
+            timer_period = ZERO_SPEED;
+            dir_M = 0;
+        }
+        else
+        {
+            dir_M = 1;
+
+            if (motorNum == 1)
+                SET(DIR_M1_PORT, DIR_M1_PIN);
+            else
+                CLR(DIR_M2_PORT, DIR_M2_PIN);
+        }
     }
     else
     {
-        timer_period = 2000000 / -speed;
-        dir_M = -1;
-        if (motorNum == 1) CLR(DIR_M1_PORT, DIR_M1_PIN);
-        else               SET(DIR_M2_PORT, DIR_M2_PIN);
+        timer_period = 2000000L / -speed;
+
+        if (timer_period > 65535L)
+        {
+            timer_period = ZERO_SPEED;
+            dir_M = 0;
+        }
+        else
+        {
+            dir_M = -1;
+
+            if (motorNum == 1)
+                CLR(DIR_M1_PORT, DIR_M1_PIN);
+            else
+                SET(DIR_M2_PORT, DIR_M2_PIN);
+        }
     }
 
-    if (timer_period > 65535)
-        timer_period = ZERO_SPEED;
+    uint16_t ocr = (uint16_t)timer_period;
 
     if (motorNum == 1)
     {
-        OCR1A = timer_period;
-        if (TCNT1 > OCR1A) TCNT1 = 0;
+        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+        {
+            OCR1A = ocr;
+            if (TCNT1 > OCR1A)
+                TCNT1 = 0;
+        }
     }
     else
     {
-        OCR3A = timer_period;
-        if (TCNT3 > OCR3A) TCNT3 = 0;
+        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+        {
+            OCR3A = ocr;
+            if (TCNT3 > OCR3A)
+                TCNT3 = 0;
+        }
     }
 }
